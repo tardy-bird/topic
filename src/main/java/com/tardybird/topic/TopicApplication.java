fa915ca3211fdcd7ed320e328c204abe2ad0d46b
seekincs <seekincs@gmail.com>
2019-12-06 20:57:33 +0800

    init Topic project.

---
+++ src/main/java/com/tardybird/topic/TopicApplication.java
@@ -0,0 +1,16 @@
+package com.tardybird.topic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author nick
+ */
+@SpringBootApplication
+public class TopicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopicApplication.class, args);
+    }
+
+}
