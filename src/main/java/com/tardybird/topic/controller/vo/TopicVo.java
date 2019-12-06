e7b9289438fab173f816caf0382fc75d20fd27ee
seekincs <seekincs@gmail.com>
2019-12-06 22:31:15 +0800

    add TopicVo.

---
+++ src/main/java/com/tardybird/topic/controller/vo/TopicVo.java
@@ -0,0 +1,18 @@
+package com.tardybird.topic.controller.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author nick
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TopicVo {
+    private String picUrlList;
+    private String content;
+}
