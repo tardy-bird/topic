d440457391e3dc4806d68574a87ac5d14fab3b02
seekincs <seekincs@gmail.com>
2019-12-08 18:00:49 +0800

    fix sql statement faults.

--- src/main/java/com/tardybird/topic/mapper/TopicMapper.java
+++ src/main/java/com/tardybird/topic/mapper/TopicMapper.java
@@ -19,13 +19,13 @@ public interface TopicMapper {
      * @param id topic id
      * @return a topic
      */
-    @Select("select gmt_create,gmt_modified,is_deleted,pic_url_list,content " +
+    @Select("select id,gmt_create,gmt_modified,is_deleted,pic_url_list,content " +
             "from topic where id=#{id}")
     @Results(id = "topic", value = {
             @Result(property = "id", column = "id", javaType = Integer.class),
             @Result(property = "gmtCreate", column = "gmt_create", javaType = LocalDateTime.class),
             @Result(property = "gmtModified", column = "gmt_modified", javaType = LocalDateTime.class),
-            @Result(property = "beDeleted", column = "is_deleted", javaType = Integer.class),
+            @Result(property = "beDeleted", column = "is_deleted", javaType = Boolean.class),
             @Result(property = "picUrlList", column = "pic_url_list", javaType = String.class),
             @Result(property = "content", column = "content", javaType = String.class),
     })
@@ -46,9 +46,9 @@ public interface TopicMapper {
      *
      * @param topic topic object
      */
-    @Update("update topic(gmt_create,gmt_modified,is_deleted,is_deleted,pic_url_list,content)" +
-            "set gmt_create=#{gmtCreate},gmt_modified=#{gmtModified},is_deleted=#{BeDeleted}," +
-            "pic_url_list=#{picUrlList},content=#(content) where id=#{id}")
+    @Update("update topic " +
+            "set gmt_create=#{gmtCreate},gmt_modified=#{gmtModified},is_deleted=#{beDeleted}," +
+            "pic_url_list=#{picUrlList},content=#{content} where id=#{id}")
     void updateTopic(Topic topic);
 
     /**
