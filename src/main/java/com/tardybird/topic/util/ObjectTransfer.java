2ef00cb4cae8b821372f30a63af85c629bcae1b0
seekincs <seekincs@gmail.com>
2019-12-12 21:10:58 +0800

    add Topic and TopicPo transfer.

---
+++ src/main/java/com/tardybird/topic/util/ObjectTransfer.java
@@ -0,0 +1,36 @@
+package com.tardybird.topic.util;
+
+import com.tardybird.topic.domain.Topic;
+import com.tardybird.topic.po.TopicPo;
+
+/**
+ * @author nick
+ */
+public class ObjectTransfer {
+
+    public static Topic topicPo2Topic(TopicPo topicPo) {
+        Topic topic = new Topic();
+
+        topic.setId(topicPo.getId());
+        topic.setGmtCreate(topicPo.getGmtCreate());
+        topic.setGmtModified(topicPo.getGmtModified());
+        topic.setBeDeleted(topicPo.getBeDeleted());
+        topic.setPicUrlList(topicPo.getPicUrlList());
+        topic.setContent(topicPo.getContent());
+
+        return topic;
+    }
+
+    public static TopicPo topic2TopicPo(Topic topic) {
+        TopicPo topicPo = new TopicPo();
+
+        topicPo.setId(topic.getId());
+        topicPo.setGmtCreate(topic.getGmtCreate());
+        topicPo.setGmtModified(topic.getGmtModified());
+        topicPo.setBeDeleted(topic.getBeDeleted());
+        topicPo.setPicUrlList(topic.getPicUrlList());
+        topicPo.setContent(topic.getContent());
+
+        return topicPo;
+    }
+}
