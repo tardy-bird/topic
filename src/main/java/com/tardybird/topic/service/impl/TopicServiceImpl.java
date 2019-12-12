f002cf26f222956028fe1eae3a7321da692756c6
seekincs <seekincs@gmail.com>
2019-12-12 21:10:32 +0800

    complete TopicServiceImpl.java

--- src/main/java/com/tardybird/topic/service/impl/TopicServiceImpl.java
+++ src/main/java/com/tardybird/topic/service/impl/TopicServiceImpl.java
@@ -4,9 +4,12 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.tardybird.topic.domain.Topic;
 import com.tardybird.topic.mapper.TopicMapper;
+import com.tardybird.topic.po.TopicPo;
 import com.tardybird.topic.service.TopicService;
+import com.tardybird.topic.util.ObjectTransfer;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -24,29 +27,49 @@ public class TopicServiceImpl implements TopicService {
 
     @Override
     public Topic getTopicDetail(Integer id) {
-        return topicMapper.getTopicDetail(id);
+        TopicPo topicPo = topicMapper.getTopicDetail(id);
+
+        Topic topic = ObjectTransfer.topicPo2Topic(topicPo);
+
+        // 图片URL已经隐含在picUrlList中
+        topic.setPictures(null);
+        return topic;
     }
 
     @Override
     public Object getTopics(Integer page, Integer limit) {
+
         PageHelper.startPage(page, limit);
-        List<Topic> topics = topicMapper.getTopics();
+
+        List<TopicPo> topicPos = topicMapper.getTopics();
+        List<Topic> topics = new ArrayList<>();
+
+        for (TopicPo topicPo : topicPos) {
+            Topic topic = ObjectTransfer.topicPo2Topic(topicPo);
+            topic.setPictures(null);
+
+            topics.add(topic);
+        }
+
         return new PageInfo<>(topics);
     }
 
     @Override
     public void addTopic(Topic topic) {
-        topicMapper.addTopic(topic);
+        TopicPo topicPo = ObjectTransfer.topic2TopicPo(topic);
+        topicMapper.addTopic(topicPo);
     }
 
     @Override
     public void updateTopic(Topic topic) {
-        topicMapper.updateTopic(topic);
+        TopicPo topicPo = ObjectTransfer.topic2TopicPo(topic);
+        topicMapper.updateTopic(topicPo);
     }
 
     @Override
     public void deleteTopic(Topic topic) {
-        topicMapper.deleteTopic(topic);
+        TopicPo topicPo = ObjectTransfer.topic2TopicPo(topic);
+        topicMapper.deleteTopic(topicPo);
     }
 
 }
